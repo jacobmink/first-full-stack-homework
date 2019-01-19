23303c2ec0e505e099329064583082d4b883ff58
Jacob Mink <jacobmink@gmail.com>
2019-01-19 10:35:36 -0700

    let's try this again

--- models/badges_db.js
+++ models/badges_db.js
@@ -9,9 +9,7 @@ const badgeSchema = new Schema({
     title: {type: String, required: true},
     badgeType: {type: String, required: true},
     colors: [{type: String, required: true}],
-    difficulty: {type: Number, required: true},
-    
-
+    difficulty: {type: Number, required: true}
 });
 
 
